97c77cefb22051ff8e1324047410af2a1aed488b
lasheda <144438920+lasheda@users.noreply.github.com>
2025-09-10 18:13:17 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('dance-memory-coach').then((cache) => {
+      return cache.addAll([
+        '/Dance-Memory-Pretty/',
+        '/Dance-Memory-Pretty/index.html',
+        '/Dance-Memory-Pretty/styles.css',
+        '/Dance-Memory-Pretty/script.js',
+        '/Dance-Memory-Pretty/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+
+  // Solo interceptar peticiones http(s)
+  if (url.protocol !== 'http:' && url.protocol !== 'https:') {
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
